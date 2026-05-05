54167bdba334ee3d907e7e8dcc5a2d16ac03194c
OrenLiu <deanliusy@163.com>
2026-05-05 14:28:45 +0800

    调整一些注释

--- soup3D/__init__.py
+++ soup3D/__init__.py
@@ -528,8 +528,8 @@ def open_mtl(mtl: str,
     :param mtl:             *.mtl纹理文件路径
     :param double_side:     是否启用双面渲染
     :param roll_funk:       每当读取一行时调用一次，方法需有，且仅有1个参数，用于接收已读取的行数
-    :param encoding:        读取mtl文件时使用的字符集
-    :param max_light_count: 这些着色器出现时会同时出现的最多的光源数量
+    :param encoding:        读取文本文件时使用的字符集(建议在建模软件里把所有元素命名为英文，这样就不用管这个参数了)
+    :param max_light_count: 这些着色器出现时会同时出现的最多的光源数量，大了会导致性能问题
     :param surface:         模型使用的表面着色器类型，着色器需要有base_color, emission, normal, double_side, max_light_count等
                             参数
     :return: 所有生成出的表面着色器
@@ -640,8 +640,8 @@ def open_obj(obj: str,
     :param mtl:             *.mtl纹理文件路径或已加载的材质字典
     :param double_side:     是否启用双面渲染
     :param roll_funk:       每当读取一行时调用一次，方法需有，且仅有1个参数，用于接收已读取的行数
-    :param encoding:        读取obj或mtl文件时使用的字符集
-    :param max_light_count: 该模型出现时会同时出现的最多的光源数量
+    :param encoding:        读取文本文件时使用的字符集(建议在建模软件里把所有元素命名为英文，这样就不用管这个参数了)
+    :param max_light_count: 该模型出现时会同时出现的最多的光源数量，大了会导致性能问题
     :return: 生成出来的模型数据(Model类)
     """
     # 处理mtl文件
@@ -830,8 +830,8 @@ def open_gltf(
     :param gltf:            gltf模型文件路径
     :param double_side:     是否启用双面渲染
     :param roll_funk:       每当读取一行时调用一次，方法需有，且仅有1个参数，用于接收已读取的行数
-    :param encoding:        读取obj或mtl文件时使用的字符集
-    :param max_light_count: 该模型出现时会同时出现的最多的光源数量
+    :param encoding:        读取文本文件时使用的字符集(建议在建模软件里把所有元素命名为英文，这样就不用管这个参数了)
+    :param max_light_count: 该模型出现时会同时出现的最多的光源数量，大了会导致性能问题
     :param surface:         模型使用的表面着色器类型，着色器需要有skeleton, base_color, emission, normal, double_side,
                             max_light_count等参数
     :return: 模型数据(Model类), 骨架数据(字典)
--- soup3D/shader.py
+++ soup3D/shader.py
@@ -1163,7 +1163,11 @@ class BoneBinderSP(AutoSP):
         """
         创建该着色器的渲染流程
         :param mode:   绘制方式
-        :param vertex: 表面中所有的顶点
+        :param vertex: 表面中所有的顶点，格式：
+                       [
+                           ({name: weight, ...}, x, y, z, u, v) | ({name: weight, ...}, weight, x, y, z, u, v, nx, ny, nz),
+                           ...
+                       ]
         :return: None
         """
         ...
